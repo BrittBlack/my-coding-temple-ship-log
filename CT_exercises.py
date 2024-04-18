a40160b0cffe759edacaeec3ad9f974804021f70
BrittBlack <97366121+BrittBlack@users.noreply.github.com>
2024-04-18 10:25:15 -0500

    CT exercises/module

---
+++ CT_exercises.py
@@ -0,0 +1,22 @@
+#This is a print
+print('this is a comment')
+
+#This is my variable
+a = "hi!"
+
+#This is a condition
+if 9 > 5:
+    print(a)
+    
+
+#Topic 1; Module 2
+favorite_quote = '''Life is like 
+a box of 
+chocolates'''
+
+print(favorite_quote)
+
+#print the sum of the numbers added
+
+sum = 3.14 + 3.14
+print( "The sum of 3.14 and 3.14 is:", sum)
