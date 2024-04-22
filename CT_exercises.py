f06cf4fcd9d475a7897605dda9715c315cb86478
BrittBlack <97366121+BrittBlack@users.noreply.github.com>
2024-04-22 07:42:48 -0500

    updated exercises

--- CT_exercises.py
+++ CT_exercises.py
@@ -23,3 +23,113 @@ print(favorite_quote)
 sum = 3.14 + 3.14
 print( "The sum of 3.14 and 3.14 is:", sum)
 
+
+
+#Topic 3, Module 2
+#1
+my_full_name= "Jane Doe"
+print(my_full_name)
+
+#2 
+#myName is a var that holds a first name 
+#myname is a var that holds a last name
+myName= "Brittney"
+myname= "Blackmon"
+
+print(myName)
+print(myname)
+
+#3
+fav_color = "Blue"
+print(fav_color)
+
+#4
+
+fruit1, fruit2, fruit3 = "apple", "banana", "cherry"
+
+print(fruit1)
+print(fruit2)
+print(fruit3)
+
+#5 
+integer_var = 30
+float_var = 3.14
+string_var = "Bye Felicia"
+
+print(integer_var, type(integer_var))
+print(float_var, type(float_var))
+print(string_var, type(string_var))
+
+
+#6
+mystery_variable = "1234"
+
+variable_type = type(mystery_variable)
+
+print(variable_type)
+
+#7
+dynamicVar= 3000
+print(dynamicVar, type(dynamicVar))
+
+dynamicVar= "Python is great"
+print(dynamicVar, type(dynamicVar))
+
+#8
+#if cannot be a var
+
+#9
+PI = 3.14159
+
+
+#Topic 4, Module 2
+
+#1
+
+flour_needed_percake= 250
+flour_instock= 2500
+nb_cakes = flour_instock/flour_needed_percake
+
+print(nb_cakes)
+
+#2
+var_kingdom= "Pythonland"
+
+#3
+shirt1 = 45
+shirt2 = 50
+shirt_costdifference = shirt2 >= shirt1
+
+print(shirt_costdifference)
+
+#4
+maybe_rain = "no umbrella"
+heavy_rain = "yes umbrella"
+bring_umbrella = maybe_rain or heavy_rain
+
+print(bring_umbrella)
+
+#5
+
+Which_nbfirst = 3+5*2-8 #PEMDAS
+print("The sum equals:", Which_nbfirst)
+
+#6
+
+nb_pastries = 10
+nb_friends = 3
+nb_pastries_per_friend = nb_pastries // nb_friends
+nb_pastries_remain = ((nb_pastries - nb_pastries_per_friend* nb_friends))
+print("How many pastries remain?", nb_pastries_remain)
+print("How many pastries does each friend get?", nb_pastries_per_friend)
+
+#7 
+
+print(var_kingdom + " is wonderful")
+
+#8
+knight1 = 45
+knight2 = 50
+Thou_greater= knight2 >= knight1
+
+print(Thou_greater)
